f7f4fcbc75c2a4d8d48659090745a35ff73216a2
Гамкартиев Ибрагим Микаилович <gamkartiev@b.local>
2020-08-31 12:37:42 +0300

    work for getInsert

--- models/flightsmodels.php
+++ models/flightsmodels.php
@@ -2,13 +2,11 @@
 
 class Flights extends Base
 {
-	public function sqlGetAllString() {
-
+	public function getSelect() {
 		$table = 'flights';
 		$rows = '*';
 		$order = 'id DESC';
 		$where = '';
-
 
 		$base = new Base();
 		$result = $base->select($table, $rows, $where, $order);
@@ -16,10 +14,23 @@ class Flights extends Base
 		return $result;
 	}
 
-	protected function sqlCreateString($place_1, $place_2, $date_1, $date_2, $freight, $weight, $volume, $cost, $form_of_payment, $proxy, $request, $note) {
+	protected function getInsert($place_1, $place_2, $date_1, $date_2, $freight, $weight, $volume, $cost, $form_of_payment, $proxy, $request, $note) {
+		// $place_1 = $_POST['place_1'];
+		// $place_2 = $_POST['place_2'];
+		// $date_1 = $_POST['date_1'];
+		// $date_2 = $_POST['date_2'];
+		// $freight = $_POST['freight'];
+		// $weight = $_POST['weight'];
+		// $volume = $_POST['volume'];
+		// $cost = $_POST['cost'];
+		// $form_of_payment = $_POST['form_of_payment'];
+		// $proxy = $_POST['proxy'];
+		// $request = $_POST['request'];
+		// $note = $_POST['note'];
 
-		$sql = "INSERT INTO `flights`(`place_1`, `place_2`, `date_1`, `date_2`, `freight`, `weight`, `volume`, `cost`, `form_of_payment`, `proxy`, `request`, `note`, `id_customers`, `id_cars`)
-												VALUES ('$place_1', '$place_2', '$date_1', '$date_2', '$freight', '$weight', '$volume', '$cost', '$form_of_payment', '$proxy', '$request',	 '$note', '1', '1')";
+
+		`place_1`, `place_2`, `date_1`, `date_2`, `freight`, `weight`, `volume`, `cost`, `form_of_payment`, `proxy`, `request`, `note`, `id_customers`, `id_cars`
+		'$place_1', '$place_2', '$date_1', '$date_2', '$freight', '$weight', '$volume', '$cost', '$form_of_payment', '$proxy', '$request',	 '$note', '1', '1'
 		return $sql;
 	}
 
