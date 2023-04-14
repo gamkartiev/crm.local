19a00dae135e0a5008cb624288f7a356aa6eaca1
Гамкартиев Ибрагим Микаилович <gamkartiev@b.local>
2023-04-14 18:44:54 +0300

    worked in Salary page for Drivers Other Works

--- views/driversOtherWorks/driversOtherWorksForm.php
+++ views/driversOtherWorks/driversOtherWorksForm.php
@@ -27,51 +27,6 @@
       <input type="date" name="date_of_work" value="">  <br /><br />
     </label>
 
-    <label> Время нарушения
-      <select class="" name="">
-        <option value=""> Не выплачено </option>
-        <option value=""> Выплачено </option>
-      </select>
-      <input type="time" name="status" value="">  <br /><br />
-    </label>
-
-    <label> Машина:
-     <select name="id_cars" required>
-        <option disabled>Выберите машину:</option>
-        <?php
-        foreach($cars as $a): ?>
-        <option value="<?= $a['id'] ?>"> <?= $a['state_sign_cars'] ?></option>
-        <<?php endforeach; ?>
-     </select>
-     </label> <br /><br />
-
-    <label> Месяц, год удержания
-      <input type="date" name="hold_date" value="">  <br /><br />
-    </label>
-
-    <label> Удержано с водителя
-      <input type="text" name="withheld" value="250">  <br /><br />
-    </label>
-
-    <label> Сумма к оплате
-      <input type="text" name="to_pay" value="250">  <br /><br />
-    </label>
-
-    <label> Срок оплаты
-      <input type="date" name="due_date" value="">  <br /><br />
-    </label>
-
-    <label> Сумма после срока оплаты
-      <input type="text" name="after_the_due_date" value="500">  <br /><br />
-    </label>
-
-    <label> Дата оформления заявки
-      <input type="date" name="date_of_application" value="<?= date("Y-m-d") ?>">  <br /><br />
-    </label>
-
-    <label> Примечаение
-      <input type="text" name="note" value="---">  <br /><br />
-    </label>
 
     <label> Оплата
      <select name="status" required>
@@ -81,6 +36,13 @@
        <?php endforeach; ?>
      </select>
     </label> <br /><br />
+
+
+    <label> Примечаение
+      <input type="text" name="note" value="---">  <br /><br />
+    </label>
+
+
 
     <button type="submit" name="button"> Добавить </button>
   </form>
